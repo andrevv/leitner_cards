414e1953a6a334b9618c911b00b5bbc42f9e3b75
Andrey Vladykov <avladykov@outlook.com>
2021-08-22 14:59:39 +0300

    Refactor tests.

--- test_flashcard.py
+++
@@ -1,7 +0,0 @@
-from models import Flashcard
-
-
-def test_init():
-    card = Flashcard(question='What is the capital of Italy?', answer='Rome')
-    assert card.question == 'What is the capital of Italy?'
-    assert card.answer == 'Rome'
