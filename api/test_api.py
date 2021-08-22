ca16ba6701e600a48f4715bc9c19987feee64b11
Andrey Vladykov <avladykov@outlook.com>
2021-08-22 15:20:57 +0300

    Fixed a test.

--- api/test_api.py
+++ api/test_api.py
@@ -14,7 +14,16 @@ def client():
 
 def test_api_cards(client):
     db_fd, db_path = tempfile.mkstemp()
-    rv = client.get('/api/cards')
-    assert rv.data == b'cards'
+    resp = client.get('/api/flashcards')
+    assert resp.status_code == 200
+    assert resp.json == [{
+        'id': 1,
+        'question': 'What is the capital of Germany?',
+        'answer': 'Berlin'
+    }, {
+        'id': 2,
+        'question': 'What is the capital of France?',
+        'answer': 'Paris'
+    }]
     os.close(db_fd)
     os.unlink(db_path)
