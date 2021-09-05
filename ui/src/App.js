72c5310aa12ca62455da2750a99eab570eb847b4
Andrey Vladykov <avladykov@outlook.com>
2021-09-05 11:05:45 +0300

    Move flashcard list to a separate component.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,59 +1,12 @@
-import { useEffect, useState } from 'react';
 import './App.css'
-import './Flashcard'
-import Flashcard from './Flashcard';
+import FlashcardList from './FlashcardList';
 
 function App() {
-  const [flashcards, setFlashcards] = useState([])
-  const [question, setQuestion] = useState()
-  const [answer, setAnswer] = useState()
-  useEffect(() => {
-    fetch('http://localhost:5000/api/flashcards')
-      .then(resp => resp.json())
-      .then(data => setFlashcards(data))
-  }, [])
   return (
     <div className="container mx-auto">
-      <fieldset className="my-1">
-        <label htmlFor="question">Question: </label>
-        <input type="text" id="question" className="border rounded" onChange={e => setQuestion(e.target.value)} />
-      </fieldset>
-      <fieldset className="my-1">
-        <label htmlFor="answer">Answer: </label>
-        <input type="text" id="answer" className="border rounded" onChange={e => setAnswer(e.target.value)} />
-      </fieldset>
-      <button
-        className="border rounded-lg shadow p-3 bg-green-500 hover:bg-green-700 text-white"
-        onClick={() => addFlashcard(question, answer)}>
-          Add a Flashcard
-      </button>
-      <div className="flex">
-        {flashcards.map(flashcard =>
-          <Flashcard
-          key={flashcard.id}
-          question={flashcard.question}
-          answer={flashcard.answer} />)}
-      </div>
+      <FlashcardList />
     </div>
   )
-
-  function addFlashcard(question, answer) {
-    fetch('http://localhost:5000/api/flashcards', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        question: question,
-        answer: answer
-      })
-    })
-    .then(() => {
-      fetch('http://localhost:5000/api/flashcards')
-        .then(resp => resp.json())
-        .then(data => setFlashcards(data))
-    })
-  }
 }
 
 export default App
---
+++ ui/src/FlashcardList.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+import './Flashcard'
+import Flashcard from './Flashcard';
+
+function FlashcardList() {
+  const [flashcards, setFlashcards] = useState([])
+  const [question, setQuestion] = useState()
+  const [answer, setAnswer] = useState()
+  
+  useEffect(() => {
+    fetch('http://localhost:5000/api/flashcards')
+      .then(resp => resp.json())
+      .then(data => setFlashcards(data))
+  }, [])
+
+  return (
+		<>
+			<div className="my-4">  
+				<label className="block">
+					<span className="text-gray-700">Question: </span>
+					<input type="text" id="question" className="m-1 block w-2/4" onChange={e => setQuestion(e.target.value)} value={question} />
+				</label>
+				<label className="block">
+					<span className="text-gray-700">Answer: </span>
+					<input type="text" id="answer" className="m-1 block w-2/4" onChange={e => setAnswer(e.target.value)} value={answer} />
+				</label>
+				<button
+					className="border rounded-lg shadow p-3 bg-green-500 hover:bg-green-700 text-white mt-2"
+					onClick={() => addFlashcard(question, answer)}>
+						Add a Flashcard
+				</button>
+			</div>
+			<div className="flex">
+				{flashcards.map(flashcard =>
+					<Flashcard
+						key={flashcard.id}
+						question={flashcard.question}
+						answer={flashcard.answer} />)}
+			</div>
+		</>
+  )
+
+  function addFlashcard(question, answer) {
+    fetch('http://localhost:5000/api/flashcards', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        question: question,
+        answer: answer
+      })
+    })
+    .then(() => {
+      fetch('http://localhost:5000/api/flashcards')
+        .then(resp => resp.json())
+        .then(data => {
+          setFlashcards(data)
+          setQuestion('')
+          setAnswer('')
+        })
+    })
+  }
+}
+
+export default FlashcardList
