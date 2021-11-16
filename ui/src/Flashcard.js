a5b48f04e0d668e60878813895e4917f923687a8
Andrey Vladykov <avladykov@outlook.com>
2021-11-16 22:58:57 +0300

    Enhance flashcard markup.

--- ui/src/Flashcard.js
+++ ui/src/Flashcard.js
@@ -1,8 +1,8 @@
 function Flashcard(props) {
 	return (
-		<div className="m-2 p-2 border rounded shadow">
-			<h1>{props.question}</h1>
-			<h2>{props.answer}</h2>
+		<div className="ml-2 mr-2 p-5 border rounded shadow">
+			<h1>Q: {props.question}</h1>
+			<h2>A: {props.answer}</h2>
 		</div>
 	)
 }
