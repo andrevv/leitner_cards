f020989c67b42b8136afbb5a7c11078896d94c67
Andrey Vladykov <avladykov@outlook.com>
2021-11-29 01:36:46 +0300

    Require authentication.

--- api/home/views.py
+++ api/home/views.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, send_from_directory
 
+from api.auth.routes import requires_auth
+
 bp = Blueprint('home', __name__)
 
 
 @bp.route('/', methods=['GET'])
+@requires_auth
 def home():
     return send_from_directory(directory='../build', path='index.html')
