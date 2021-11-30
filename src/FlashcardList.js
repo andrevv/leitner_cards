3bcb5ad45fc47579527f4b417071f27b066005f2
Andrey Vladykov <avladykov@outlook.com>
2021-11-30 20:25:59 +0300

    Initialize flashcard list during component loading.

--- src/FlashcardList.js
+++ src/FlashcardList.js
@@ -1,66 +1,69 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import './App.css'
 import './Flashcard'
 import Flashcard from './Flashcard';
 
 function FlashcardList() {
-  const [flashcards, setFlashcards] = useState([])
-  const [question, setQuestion] = useState()
-  const [answer, setAnswer] = useState()
-  
-  useEffect(() => {
-    fetch('/api/flashcards')
-      .then(resp => resp.json())
-  }, [])
+    const [flashcards, setFlashcards] = useState([])
+    const [question, setQuestion] = useState()
+    const [answer, setAnswer] = useState()
 
-  return (
-		<>
-			<div className="my-4">  
-				<label className="block">
-					<span className="text-gray-700">Question: </span>
-					<input type="text" id="question" className="m-1 block w-2/4" onChange={e => setQuestion(e.target.value)} value={question} />
-				</label>
-				<label className="block">
-					<span className="text-gray-700">Answer: </span>
-					<input type="text" id="answer" className="m-1 block w-2/4" onChange={e => setAnswer(e.target.value)} value={answer} />
-				</label>
-				<button
-					className="border rounded-lg shadow p-3 bg-green-500 hover:bg-green-700 text-white mt-2"
-					onClick={() => addFlashcard(question, answer)}>
-						Add a flashcard
-				</button>
-			</div>
-			<div className="flex flex-wrap">
-				{flashcards.map(flashcard =>
-					<Flashcard
-						key={flashcard.id}
-						question={flashcard.question}
-						answer={flashcard.answer} />)}
-			</div>
-		</>
-  )
+    useEffect(() => {
+        fetch('/api/flashcards')
+            .then(resp => resp.json())
+            .then(data => setFlashcards(data))
+    }, [])
 
-  function addFlashcard(question, answer) {
-    fetch('/api/flashcards', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        question: question,
-        answer: answer
-      })
-    })
-    .then(() => {
-      fetch('/api/flashcards')
-        .then(resp => resp.json())
-        .then(data => {
-          setFlashcards(data)
-          setQuestion('')
-          setAnswer('')
+    return (
+        <>
+            <div className="my-4">
+                <label className="block">
+                    <span className="text-gray-700">Question: </span>
+                    <input type="text" id="question" className="m-1 block w-2/4"
+                           onChange={e => setQuestion(e.target.value)} value={question}/>
+                </label>
+                <label className="block">
+                    <span className="text-gray-700">Answer: </span>
+                    <input type="text" id="answer" className="m-1 block w-2/4" onChange={e => setAnswer(e.target.value)}
+                           value={answer}/>
+                </label>
+                <button
+                    className="border rounded-lg shadow p-3 bg-green-500 hover:bg-green-700 text-white mt-2"
+                    onClick={() => addFlashcard(question, answer)}>
+                    Add a flashcard
+                </button>
+            </div>
+            <div className="flex flex-wrap">
+                {flashcards.map(flashcard =>
+                    <Flashcard
+                        key={flashcard.id}
+                        question={flashcard.question}
+                        answer={flashcard.answer}/>)}
+            </div>
+        </>
+    )
+
+    function addFlashcard(question, answer) {
+        fetch('/api/flashcards', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                question: question,
+                answer: answer
+            })
         })
-    })
-  }
+            .then(() => {
+                fetch('/api/flashcards')
+                    .then(resp => resp.json())
+                    .then(data => {
+                        setFlashcards(data)
+                        setQuestion('')
+                        setAnswer('')
+                    })
+            })
+    }
 }
 
 export default FlashcardList
