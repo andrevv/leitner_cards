71423b1fb725626d98cc0c0eecdde15b336be6d0
Andrey Vladykov <avladykov@outlook.com>
2021-11-30 20:16:38 +0300

    Add header markup.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
 import './App.css'
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from 'react-router-dom'
 import FlashcardList from './FlashcardList'
 import Practice from './Practice'
+import {DocumentIcon, LogoutIcon} from '@heroicons/react/outline'
 
 function App() {
-  return (
-    <div className="container mx-auto">
-      <button onClick={() => window.location = '/api/auth/logout'}>Logout</button>
-      <Router>
-        <div>
-          <ul className="flex gap-5 text-blue-700">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/practice">Practice</Link>
-            </li>
-          </ul>
+    return (
+        <>
+            <div>
+                <Router>
+                    <ul className="px-10 bg-gray-50 text-gray-700 flex items-center gap-5 py-3">
+                        <div>
+                            <Link to="/">
+                                <DocumentIcon className="w-10 h-10"/>
+                            </Link>
+                        </div>
+                        <div className="flex flex-grow">
+                            <li className="rounded-md text-sm hover:border p-2 cursor-pointer hover:bg-gray-200 hover:text-gray-900">
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className="rounded-md text-sm hover:border p-2 cursor-pointer hover:bg-gray-200 hover:text-gray-900">
+                                <Link to="/practice">Practice</Link>
+                            </li>
+                        </div>
+                        <div>
+                            <li className="rounded-md text-sm hover:border p-2 cursor-pointer hover:bg-gray-200 hover:text-gray-900">
+                                <button onClick={() => window.location = '/api/auth/logout'}>
+                                    <div className="flex items-center gap-1">
+                                        <LogoutIcon className="w-5 h-5" />
+                                        Logout
+                                    </div>
+                                </button>
+                            </li>
+                        </div>
+                    </ul>
 
-          <Switch>
-            <Route path="/practice">
-              <Practice />
-            </Route>
-            <Route path="/">
-              <FlashcardList />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  )
+                    <div className="container mx-auto">
+                        <Switch>
+                            <Route path="/practice">
+                                <Practice/>
+                            </Route>
+                            <Route path="/">
+                                <FlashcardList/>
+                            </Route>
+                        </Switch>
+                    </div>
+                </Router>
+            </div>
+        </>
+    )
 }
 
 export default App
